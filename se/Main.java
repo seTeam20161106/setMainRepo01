6c45a4e909d68fc047a6dfc2e53b160c6e1f672e
winseung76 <58281882+winseung76@users.noreply.github.com>
2019-12-03 15:22:26 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -1,9 +1,11 @@
 package se;
 
+// 1: remoted by winseung76
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
 	}
 
