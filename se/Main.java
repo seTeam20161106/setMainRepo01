197ab0433e6817c94cfb2276b8f5c59de8467f8e
winseung76 <58281882+winseung76@users.noreply.github.com>
2019-12-03 16:07:16 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -3,6 +3,7 @@ package se;
 // 1: remoted by winseung76
 // 1: local by winseung76
 // 2: remoted by winseung76
+// 1: localbranch by winseung76
 public class Main {
 
 	public static void main(String[] args) {
